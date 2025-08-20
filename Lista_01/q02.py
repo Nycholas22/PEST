977cc51fd0a9728c55ec000ab5dcef45becc9cb2
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-20 18:41:45 -0300

    Update q02.py

--- Lista_01/q02.py
+++ Lista_01/q02.py
@@ -1,1 +1,10 @@
+quantidade = 60
+preço_livro = 24.95
+desconto = 0.40
+transportePrimeiro = 3.00
+transporteAdicional = 0.75
+
+
+preço = preço_livro * (1 - desconto) * quantidade + transportePrimeiro + transporteAdicional * (quantidade - 1)
+print(preço)
 
