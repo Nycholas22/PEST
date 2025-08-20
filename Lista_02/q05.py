1feef93251b7243d654de03a5d94b28044fb6f95
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-20 18:24:07 -0300

    Update q05.py

---
+++ Lista_02/q05.py
@@ -0,0 +1,8 @@
+numero = int(input("digite um número: "))
+if numero % 2 == 0:
+    print("o número é divisível por 2")
+elif numero % 3 == 0:
+    print("o número é divisível por 3")
+else:
+    print("o número não é divisível nem por 2 nem por 3")
+    
