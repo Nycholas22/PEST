de1ada74828b9ef81ab4fb8c7a1f61b5bccd2f06
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-31 19:28:06 -0300

    Update q05.py

--- Lista_04/q05.py
+++ Lista_04/q05.py
@@ -1,1 +1,11 @@
+n = int(input("digite um número: "))
 
+if n <= 1:
+    print(f"{n} não é primo")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print(f"{n} não é primo")
+            break
+    else:
+        print(f"{n} é primo")
