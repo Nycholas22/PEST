78b43ab4ef86e12006bdb3f70c36c2d6a6af1690
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-31 19:28:50 -0300

    Update q08.py

--- Lista_04/q08.py
+++ Lista_04/q08.py
@@ -1,1 +1,7 @@
+n = int(input("Digite um número: "))
 
+for num in range(0, n + 1):
+    fatorial = 1
+    for i in range(1, num + 1):
+        fatorial *= i
+    print(f"O fatorial de {num} é {fatorial}")
