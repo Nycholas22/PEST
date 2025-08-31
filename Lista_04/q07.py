550cb0a9fee577c54c53b645d53a4d75a852cda7
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-31 19:28:35 -0300

    Update q07.py

--- Lista_04/q07.py
+++ Lista_04/q07.py
@@ -1,1 +1,8 @@
-
+soma = 0
+for n in range(2, 101):
+    for i in range(2, n):
+        if n % i == 0:
+            break
+    else:
+        soma += n
+print("a soma dos números primos entre 1 e 100 é:", soma)
