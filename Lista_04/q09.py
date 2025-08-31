71f751d47e441f2b2b9f078d3a55a58e66743bca
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-31 19:29:19 -0300

    Update q09.py

--- Lista_04/q09.py
+++ Lista_04/q09.py
@@ -1,1 +1,24 @@
+inicio = int(input("Digite o início do intervalo: "))
+fim = int(input("Digite o fim do intervalo: "))
 
+print(f"Números de Armstrong entre {inicio} e {fim}:")
+
+for numero in range(inicio, fim + 1):
+    n = 0
+    temp = numero
+    for i in range(1, numero + 1):
+        if temp == 0:
+            break
+        temp = temp // 10
+        n = n + 1
+
+    soma = 0
+
+    temp = numero
+    for i in range(n):
+        digito = temp % 10
+        soma = soma + digito ** n
+        temp = temp // 10
+
+    if soma == numero:
+        print(numero)
