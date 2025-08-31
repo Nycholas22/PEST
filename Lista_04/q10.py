220d9b67080a0511a9536e349d18e82dafab1461
Nycholas Kelvyn Braga Santos <nycholaskelvyn@aluno.ifce.edu.br>
2025-08-31 19:29:57 -0300

    Update q10.py

--- Lista_04/q10.py
+++ Lista_04/q10.py
@@ -1,1 +1,7 @@
+n = int(input("digite um número: "))
+soma = 0
 
+for i in range(1, n):
+    if i % 3 == 0 or i % 5 == 0:
+        soma += i
+print("a soma dos números menores que {n} divisiveis por 3 ou 5 é:", soma)
